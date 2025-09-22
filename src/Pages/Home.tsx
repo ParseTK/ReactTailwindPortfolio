ba1fce09b677432111dc7ffb53a2c9086a24233f
TK <tjkirchner2401@student.stcc.edu>
2025-09-21 22:22:50 -0400

    Initialized updated portfolio project using react + tailwindcss. imported vite settings. some styling

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,19 @@
+import { ThemeToggle } from "@/Components/ThemeToggle"
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/* Theme toggle */}
+            <ThemeToggle />
+        {/* Background effects */}
+
+        {/* Nvigation bar */}
+
+        {/* Main content */}
+        
+        {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
---
+++ src/Pages/NotFound.tsx
@@ -0,0 +1,10 @@
+
+export const NotFound = () => {
+  return (
+    <div>
+        NotFound
+    </div>
+  )
+}
+
+export default NotFound
